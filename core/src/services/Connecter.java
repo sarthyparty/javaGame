b037c1cec5dd4630212fca125e59af2452d6cec7
sarthyparty <sarthavader@gmail.com>
2020-08-07 17:13:05 -0500

    Some stuff

--- core/src/services/Connecter.java
+++ core/src/services/Connecter.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class Connecter {
 
     public Connecter() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mariadb://192.168.1.38:3306/DB?user=gamePlayer&password=libandgdxroid123");
+        Connection connection = (Connection) DriverManager.getConnection("jdbc:mariadb://192.168.1.38:3306/myGame?user=gamePlayer&password=libandgdxroid123");
+
     }
 }
