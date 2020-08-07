a5a6da0b61a6e57c789c4ac59d8072a991e88bcc
PreciseChain <ansh.legend12@gmil.com>
2020-08-07 17:55:15 -0500

    Added keyboard input

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -15,6 +16,10 @@ import java.sql.SQLException;
 import services.*;
 
 public class MyGdxGame implements ApplicationListener {
+	private float SpriteX=0;
+	private float SpriteY=0;
+	private float SpriteSpeed=50.0f;
+
 	SpriteBatch batch;
 	private TextureAtlas textureAtlas;
 	private Animation<TextureRegion> animation;
@@ -43,11 +48,20 @@ public class MyGdxGame implements ApplicationListener {
 
 	@Override
 	public void render () {
+
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT))
+			SpriteX -= Gdx.graphics.getDeltaTime() *SpriteSpeed;
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT))
+			SpriteX += Gdx.graphics.getDeltaTime() * SpriteSpeed;
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_UP))
+			SpriteY += Gdx.graphics.getDeltaTime() * SpriteSpeed;
+		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN))
+			SpriteY -= Gdx.graphics.getDeltaTime() * SpriteSpeed;
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		elapsedTime += Gdx.graphics.getDeltaTime();
-		batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
+		batch.draw(animation.getKeyFrame(elapsedTime, true), (int)SpriteX, (int) SpriteY);
 		batch.end();
 	}
 
