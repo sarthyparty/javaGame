cec55334e844eb9b8d3da0f999c079c9fb0e6c9e
sarthyparty <sarthavader@gmail.com>
2020-08-06 20:32:56 -0500

    Created an animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,56 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame implements ApplicationListener {
 	SpriteBatch batch;
-	Texture img;
+	private TextureAtlas textureAtlas;
+	private Animation<TextureRegion> animation;
+	private float elapsedTime = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		textureAtlas = new TextureAtlas(Gdx.files.internal("data/spritesheet.atlas"));
+		animation = new Animation(1/15f, textureAtlas.getRegions());
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		elapsedTime += Gdx.graphics.getDeltaTime();
+		batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
 		batch.end();
 	}
-	
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		textureAtlas.dispose();
 	}
 }
